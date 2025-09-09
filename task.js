e76438e67ebdec2dc6651c0d1e80d2b1e7a863ae
Lavanya <lavanyamithbavkar5@gmail.com>
2025-09-09 14:23:42 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,5 @@
+var a = "Hello, this is my first Node.js program!";
+var b = "I am excited to learn more about Node.js!";
+console.log(a);  //Hello, this is my first Node.js program!
+console.log(b);  // I am excited to learn more about Node.js!
+console.log(a + " " + b);  // Hello, this is my first Node.js program! I am excited to learn more about Node.js!
